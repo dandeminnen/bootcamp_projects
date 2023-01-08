b92b0055bb4378b9e1bd7f5a3bc95b1b98f04b90
dandeminnen <120632784+dandeminnen@users.noreply.github.com>
2023-01-08 19:47:41 +0700

    Create interactive rock-paper-scissors game

--- R_project/Pao_Ying_Choob.r
+++ R_project/Pao_Ying_Choob.r
@@ -4,9 +4,9 @@ score <- 0
 round <- 0
 
 repeat {
-print("Please enter your turn:")
+print("Choose one to get started.")
 print("1. Rock, 2. Paper, 3. Scissors or 0. Quit")
-player <- readLines("stdin", n=1)
+player <- readline("Please enter your turn: ")
 bot <- sample(1:3, size=1)
 if (player == 1 & bot == 1) {
     score <- score + 0
